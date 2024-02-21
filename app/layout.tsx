eaa8b5d36191b9188fe376433b16526836bde94c
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-02-21 19:34:10 -0300

    fixing url

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const manrope = Manrope({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('www.redisapres.cl'),
+  metadataBase: new URL('http://www.redisapres.cl'),
   title: {
     absolute: "RED ISAPRES | ATENCIÓN PERSONALIZADA",
     template: "%s  |  Red Isapres",
