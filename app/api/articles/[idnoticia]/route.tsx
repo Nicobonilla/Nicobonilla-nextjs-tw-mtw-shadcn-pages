c8170a5e671359ecc8c5e28cd03e087199513d37
Nicolas Bonilla <n.bonillamarin@gmail.com>
2024-02-24 05:57:07 -0300

    fix api articles/[idnoticia]

--- app/api/articles/[idnoticia]/route.tsx
+++ app/api/articles/[idnoticia]/route.tsx
@@ -1,6 +1,12 @@
 import conn from "../../../components/utils/postgresq_conn";
 import { NextRequest, NextResponse } from "next/server";
 
+type Article = {
+  id: number;
+  h1: string;
+  idnoticia: string;
+  urlimg: string;
+};
 // Utiliza la desestructuración para extraer métodos específicos de NextResponse
 const { json } = NextResponse;
 
@@ -8,13 +14,13 @@ const { json } = NextResponse;
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
   const idnoticia = url.pathname.split("/").pop(); // O alguna lógica similar que refleje tu estructura de ruta
-
+  
   if (idnoticia) {
     try {
-      const article = await conn`SELECT * WHERE idnoticia = ${idnoticia};`;
+      const article:Article[] = await conn`SELECT id FROM articles WHERE idnoticia = ${idnoticia};`;
       if (article) {
-        const content = await conn`SELECT * FROM content_elements WHERE article_id = ${article.id} ORDER BY content_order ASC;`
-        const fullArticle = { titles: article, content: content };
+        const content = await conn`SELECT * FROM content_elements WHERE article_id = ${article[0].id} ORDER BY content_order ASC;`
+        const fullArticle = { titles: article[0], content: content };
         // Envía la respuesta como JSON con un estado 200
         return json(
           { data: fullArticle },
